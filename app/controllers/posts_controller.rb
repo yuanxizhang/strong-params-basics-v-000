e034d068b9a4fe25ba6f39c5291e5fea3905e48e
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-03-09 00:03:18 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,27 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
-	
+	def create
+	  @post = Post.new(post_params(:title, :description))
+	  @post.save
+	  redirect_to post_path(@post)
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+	 
+	def update
+	  @post = Post.find(params[:id])
+	  @post.update(post_params(:title, :description))
+	  redirect_to post_path(@post)
+	end
+
+	private
+ 
+	def post_params(*args)
+  	params.require(:post).permit(*args)
+	end
 
 	def edit
 	  @post = Post.find(params[:id])
